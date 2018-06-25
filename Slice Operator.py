7284b7269a1b5875c8da882efe9ed1f8b2af8efd
Rashedul Islam Emon <35653087+rashedulemon@users.noreply.github.com>
2018-06-25 12:34:40 +0600

    Create Slice Operator.py

---
+++ Slice Operator.py
@@ -0,0 +1,7 @@
+S1 = "Hello Python"
+print(S1)  # Prints complete string
+print(S1[0])  # Prints first character of the string
+print(S1[2:5])  # Prints character starting from 3rd t 5th
+print(S1[2:])  # Prints string starting from 3rd character
+print(S1 * 2)  # Prints string two times
+print(S1 + "Thanks")  # Prints concatenated string
