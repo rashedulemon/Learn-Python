8ea004f76fd4abf76800d7525c23baa147c7ec78
Rashedul Islam Emon <35653087+rashedulemon@users.noreply.github.com>
2018-06-26 16:11:26 +0600

    Create Exam Grade.py

---
+++ math/Exam Grade.py
@@ -0,0 +1,16 @@
+# It's a program for calculate exam grade
+point = int(input("Enter your Exam point:"))
+if point >= 80:
+    print("Your grade is A+")
+elif point >= 70:
+    print("Your grade is A")
+elif point >= 60:
+    print("Your grade is A-")
+elif point >= 55:
+    print("Your grade is B")
+elif point >= 50:
+    print("Your grade is C")
+elif point >= 40:
+    print("Your grade is D")
+else:
+    print("Your grade is F")
